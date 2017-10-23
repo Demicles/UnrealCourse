1d25b90930eb019fb638cc84ad3e3583c58cba63
Remi Talbot <remi.talbot.d@gmail.com>
2017-10-22 22:52:11 -0400

    Unreal courses - Escape room - Adding Line_Tracer (Ray-Caster) and Debug mode

---
+++ 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,86 @@
+// Copyright Remi Talbot
+
+#include "Grabber.h"
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "Public/DrawDebugHelpers.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	/// Get the player view point this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+
+	//Log out to test
+	/*UE_LOG(LogTemp, Warning, TEXT("Player ViewPoint is Location: %s Rotation: %s"),
+		*PlayerViewPointLocation.ToString(),
+		*PlayerViewPointRotation.ToString()
+	);*/
+
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	///Draw a red trace in the world to visualize if in debug mode
+	if (bDebugActive == true)
+	{
+		DrawDebugLine(
+			GetWorld(),
+			PlayerViewPointLocation,
+			LineTraceEnd,
+			FColor(255, 0, 0),
+			false,
+			0.f,
+			0.f,
+			10.f
+		);
+	}
+
+	/// Setup query parameters
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	/// Line-trace (Ray-cast) out to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+
+	///See what we hit; NOTE When getting actor; if actor is NULL, UNREAL WILL CRASH
+	if (Hit.GetActor())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("You just hit: %s"), *Hit.GetActor()->GetName());
+	}
+}
+
